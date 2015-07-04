1b7a55c30c6a9fd7a283194c89b1fc9b08a50c26
Matt Hickford <matt.hickford@gmail.com>
2015-07-04 23:49:34 +0100

    rejig layout to allow python functions to share name with cpp functions

--- primesieve/primesieve.pyx
+++ primesieve/primesieve.pyx
@@ -1,8 +1,11 @@
 from libc.stdint cimport uint64_t, int64_t
-cimport primesieve
+cimport cpp_primesieve
+
+cpdef uint64_t count_primes(uint64_t start, uint64_t stop):
+    return cpp_primesieve.count_primes(start, stop)
 
 cpdef uint64_t count(uint64_t n):
-    return primesieve.count_primes(1, n)
+    return cpp_primesieve.count_primes(0, n)
 
-cpdef uint64_t nth(int64_t n):
-    return primesieve.nth_prime(n, 1)
+cpdef uint64_t nth_prime(int64_t n, uint64_t start = 0):
+    return cpp_primesieve.nth_prime(n, start)
