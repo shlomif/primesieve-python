f56b440bde2d9d6f33ea937a2c3c122e812f449c
Matt Hickford <matt.hickford@gmail.com>
2015-07-04 17:04:22 +0100

    extra test

--- tests/test_primesieve.py
+++ tests/test_primesieve.py
@@ -1,4 +1,10 @@
 import primesieve
     
 def test_count():
+    # from https://primes.utm.edu/howmany.html
     assert primesieve.count(100) == 25
+    assert primesieve.count(10**6) == 78498
+
+def test_nth_prime():
+    assert primesieve.nth_prime(25) == 97
+    assert primesieve.nth_prime(26) == 101
