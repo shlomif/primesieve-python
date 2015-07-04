c6b12931e768cddf54e98deabff2d969539335d3
Matt Hickford <matt.hickford@gmail.com>
2015-07-04 17:41:00 +0100

    change to c++

--- primesieve/primesieve.pyx
+++ primesieve/primesieve.pyx
@@ -1,7 +1,8 @@
+from libc.stdint cimport uint64_t, int64_t
 cimport primesieve
 
 cpdef uint64_t count(uint64_t n):
-    return primesieve_count_primes(1, n)
+    return primesieve.count_primes(1, n)
 
-cpdef uint64_t nth_prime(int64_t n):
-    return primesieve_nth_prime(n, 1)
+cpdef uint64_t nth(int64_t n):
+    return primesieve.nth_prime(n, 1)
