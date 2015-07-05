95089775eca626400f18c01a0fdd0759f3710f87
Matt Hickford <matt.hickford@gmail.com>
2015-07-05 23:06:05 +0100

    build primesieve library from setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
+from glob import glob
+
+library = ('primesieve', dict(
+    sources=glob("lib/primesieve/src/primesieve/*.cpp"),
+    include_dirs=["lib/primesieve/include"],
+    language="c++",
+    ))
+
+extension = Extension(
+        "primesieve",
+        ["primesieve/primesieve.pyx"],
+        include_dirs = ["lib/primesieve/include"],
+        language="c++",
+        )
 
 setup(
     name='primesieve',
     url = "https://github.com/hickford/primesieve-python",
     license = "MIT",
-    ext_modules = cythonize([Extension(
-        "*",
-        ["primesieve/primesieve.pyx"],
-        include_dirs = ["lib/primesieve/include"],
-        libraries = ["primesieve"],
-        language="c++",
-        )])
+    libraries = [library],
+    ext_modules = cythonize(extension)
 )
