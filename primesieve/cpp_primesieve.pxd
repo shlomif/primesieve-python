7a26f5c5e47b710bc7813ad775294d81ee15feea
Kim Walisch <kim.walisch@gmail.com>
2015-07-11 14:24:55 +0200

    Add k-tuplets counting functions

--- primesieve/cpp_primesieve.pxd
+++ primesieve/cpp_primesieve.pxd
@@ -2,10 +2,15 @@ from libc.stdint cimport uint64_t, int64_t
 from libcpp.vector cimport vector
 
 cdef extern from "primesieve.hpp" namespace "primesieve":
-    uint64_t count_primes(uint64_t start, uint64_t stop)
     uint64_t nth_prime(int64_t n, uint64_t start)
-    void generate_primes[T](uint64_t stop, vector[T]* primes)
-    void generate_n_primes[T](uint64_t n, vector[T]* primes)
+    void generate_primes[T](uint64_t start, uint64_t stop, vector[T]* primes)
+    void generate_n_primes[T](uint64_t n, uint64_t start, vector[T]* primes)
+    uint64_t count_primes(uint64_t start, uint64_t stop)
+    uint64_t count_twins(uint64_t start, uint64_t stop)
+    uint64_t count_triplets(uint64_t start, uint64_t stop)
+    uint64_t count_quadruplets(uint64_t start, uint64_t stop)
+    uint64_t count_quintuplets(uint64_t start, uint64_t stop)
+    uint64_t count_sextuplets(uint64_t start, uint64_t stop)
 
 cdef extern from "iterator.hpp" namespace "primesieve":
     cdef cppclass iterator:
--- primesieve/primesieve.pyx
+++ primesieve/primesieve.pyx
@@ -1,33 +1,73 @@
 from libc.stdint cimport uint64_t, int64_t
 from libcpp.vector cimport vector
 cimport cpp_primesieve
- 
-cpdef uint64_t count_primes(uint64_t start, uint64_t stop):
-    """Count primes start <= p <= stop"""
-    return cpp_primesieve.count_primes(start, stop)
 
-cpdef uint64_t count(uint64_t n):
-    """Count primes <= n"""
-    return cpp_primesieve.count_primes(0, n)
+cpdef vector[uint64_t] generate_primes(uint64_t a, uint64_t b = 0):
+    """Generate a list of primes"""
+    cdef vector[uint64_t] primes
+    if b != 0
+        cpp_primesieve.generate_primes[uint64_t](a, b, &primes)
+    else
+        cpp_primesieve.generate_primes[uint64_t](b, a, &primes)
+    return primes
+
+cpdef vector[uint64_t] generate_n_primes(uint64_t n, uint64_t start = 0):
+    """List the first n primes"""
+    cdef vector[uint64_t] primes
+    cpp_primesieve.generate_n_primes[uint64_t](n, start, &primes)
+    return primes
+
 
 cpdef uint64_t nth_prime(int64_t n, uint64_t start = 0):
     """Find the nth prime after start"""
     return cpp_primesieve.nth_prime(n, start)
 
-cpdef vector[uint64_t] generate_primes(uint64_t stop):
-    """List all primes <= stop"""
-    cdef vector[uint64_t] primes
-    cpp_primesieve.generate_primes[uint64_t](stop, &primes)
-    return primes
 
-cpdef vector[uint64_t] generate_n_primes(uint64_t n):
-    """List the first n primes"""
-    cdef vector[uint64_t] primes
-    cpp_primesieve.generate_n_primes[uint64_t](n, &primes)
-    return primes
+cpdef uint64_t count_primes(uint64_t a, uint64_t b = 0):
+    """Count prime numbers"""
+    if b != 0
+        return cpp_primesieve.count_primes(a, b)
+    else
+        return cpp_primesieve.count_primes(b, a)
+ 
+cpdef uint64_t count_twins(uint64_t a, uint64_t b = 0):
+    """Count twin primes"""
+    if b != 0
+        return cpp_primesieve.count_twins(a, b)
+    else
+        return cpp_primesieve.count_twins(b, a)
+ 
+cpdef uint64_t count_triplets(uint64_t a, uint64_t b = 0):
+    """Count prime triplets"""
+    if b != 0
+        return cpp_primesieve.count_triplets(a, b)
+    else
+        return cpp_primesieve.count_triplets(b, a)
+
+cpdef uint64_t count_quadruplets(uint64_t a, uint64_t b = 0):
+    """Count prime quadruplets"""
+    if b != 0
+        return cpp_primesieve.count_quadruplets(a, b)
+    else
+        return cpp_primesieve.count_quadruplets(b, a)
+
+cpdef uint64_t count_quintuplets(uint64_t a, uint64_t b = 0):
+    """Count prime quintuplets"""
+    if b != 0
+        return cpp_primesieve.count_quintuplets(a, b)
+    else
+        return cpp_primesieve.count_quintuplets(b, a)
+
+cpdef uint64_t count_sextuplets(uint64_t a, uint64_t b = 0):
+    """Count prime sextuplets"""
+    if b != 0
+        return cpp_primesieve.count_sextuplets(a, b)
+    else
+        return cpp_primesieve.count_sextuplets(b, a)
+
 
 cdef class Iterator:
-    cdef cpp_primesieve.iterator _iterator # should this be pointer?
+    cdef cpp_primesieve.iterator _iterator
     def __cinit__(self):
         self._iterator = cpp_primesieve.iterator()
     cpdef uint64_t next_prime(self):
