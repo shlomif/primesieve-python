c1e961eba3685c5f6b18c6e1594cd4533c450df6
Kim Walisch <kim.walisch@gmail.com>
2015-07-11 14:40:59 +0200

    Update test_primesieve.py

--- tests/test_primesieve.py
+++ tests/test_primesieve.py
@@ -1,19 +1,23 @@
 import primesieve
-    
-def test_count():
-    # from https://primes.utm.edu/howmany.html
-    assert primesieve.count(100) == 25
-    assert primesieve.count(10**6) == 78498
+
+def test_count_primes():
+    assert primesieve.count_primes(100) == 25
+    assert primesieve.count_primes(10**6) == 78498
+    assert primesieve.count_primes(100, 10**6) == 78473
 
 def test_nth_prime():
     assert primesieve.nth_prime(25) == 97
     assert primesieve.nth_prime(26) == 101
+    assert primesieve.nth_prime(1, 100) == 101
+    assert primesieve.nth_prime(100, 1000000) == 1001311
 
 def test_generate_primes():
     assert primesieve.generate_primes(10) == [2,3,5,7]
+    assert primesieve.generate_primes(10, 20) == [11,13,17,19]
 
 def test_generate_n_primes():
     assert primesieve.generate_n_primes(7) == [2,3,5,7,11,13,17]
+    assert primesieve.generate_n_primes(5, 100) == [101,103,107,109,113]
 
 def test_iterator():
     it = primesieve.Iterator()
