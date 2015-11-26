af5ecf37c0b1ba4a294f6f09ac92baf36aec8e22
Kim Walisch <kim.walisch@gmail.com>
2015-11-26 21:45:53 +0100

    Use int64_t instead if uint64_t

--- primesieve/numpy/core.pyx
+++ primesieve/numpy/core.pyx
@@ -1,4 +1,4 @@
-from libc.stdint cimport uint64_t, int64_t
+from libc.stdint cimport int64_t
 cimport cpp_core
 import numpy as np
 cimport numpy as np
@@ -7,41 +7,32 @@ np.import_array()
 
 cdef extern from "primesieve.h":
     cpdef enum:
-        SHORT_PRIMES
-        USHORT_PRIMES
-        INT_PRIMES
-        UINT_PRIMES
-        LONG_PRIMES
-        ULONG_PRIMES
-        LONGLONG_PRIMES
-        ULONGLONG_PRIMES
-        INT16_PRIMES
-        UINT16_PRIMES
-        INT32_PRIMES
-        UINT32_PRIMES
         INT64_PRIMES
-        UINT64_PRIMES
 
 cdef extern from "numpy/arrayobject.h":
     void PyArray_ENABLEFLAGS(np.ndarray arr, int flags)
 
 cdef c_to_numpy_array(void* ptr, np.npy_intp N, int t):
     """Bind C array allocated using malloc to NumPy ndarray"""
-    cdef np.ndarray[np.uint64_t, ndim=1] arr = np.PyArray_SimpleNewFromData(1, &N, t, ptr)
+    cdef np.ndarray[np.int64_t, ndim=1] arr = np.PyArray_SimpleNewFromData(1, &N, t, ptr)
     PyArray_ENABLEFLAGS(arr, np.NPY_OWNDATA)
     return arr
 
-cpdef np.ndarray[np.uint64_t, ndim=1] generate_primes(uint64_t a, uint64_t b = 0) except +:
+cpdef np.ndarray[np.int64_t, ndim=1] generate_primes(int64_t a, int64_t b = 0) except +:
     """Generate a numpy primes array"""
+    a = max(a, 0)
+    b = max(b, 0)
     if b == 0:
         (a,b) = (0,a)
     cdef size_t size = 0
-    cdef void* c_primes = cpp_core.primesieve_generate_primes(a, b, &size, UINT64_PRIMES)
-    primes = c_to_numpy_array(c_primes, size, np.NPY_UINT64)
+    cdef void* c_primes = cpp_core.primesieve_generate_primes(a, b, &size, INT64_PRIMES)
+    primes = c_to_numpy_array(c_primes, size, np.NPY_INT64)
     return primes
 
-cpdef np.ndarray[np.uint64_t, ndim=1] generate_n_primes(uint64_t n, uint64_t start = 0) except +:
+cpdef np.ndarray[np.int64_t, ndim=1] generate_n_primes(int64_t n, int64_t start = 0) except +:
+    n = max(n, 0)
+    start = max(start, 0)
     """Generate a numpy array with the next n primes"""
-    cdef void* c_primes = cpp_core.primesieve_generate_n_primes(n, start, UINT64_PRIMES)
-    primes = c_to_numpy_array(c_primes, n, np.NPY_UINT64)
+    cdef void* c_primes = cpp_core.primesieve_generate_n_primes(n, start, INT64_PRIMES)
+    primes = c_to_numpy_array(c_primes, n, np.NPY_INT64)
     return primes
