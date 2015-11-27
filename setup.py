3366c49183879f5371901324724cd038bd0cc020
Kim Walisch <kim.walisch@gmail.com>
2015-11-27 23:14:58 +0100

    Revert back

--- setup.py
+++ setup.py
@@ -1,27 +1,17 @@
 from setuptools import setup, Extension
-from distutils.command.build_ext import build_ext
 from glob import glob
+from distutils.command.build_ext import build_ext
+from distutils.command.build_clib import build_clib
 import os
 import shutil
 import subprocess
 import tempfile
 
-# --------------------- Initialization ------------------------------
-
-libprimesieve_sources = glob("lib/primesieve/src/primesieve/*.cpp")
 extensions = []
 extra_compile_args = []
 extra_link_args = []
 
-# Use Cython if available.
-try:
-    from Cython.Build import cythonize
-except ImportError:
-    cythonize = None
-
-# --------------------- Get OpenMP compiler flag --------------------
-
-# If this C test program compiles the compiler supports OpenMP
+# If this C/C++ test program compiles the compiler supports OpenMP
 # http://stackoverflow.com/questions/16549893/programatically-testing-for-openmp-support-from-a-python-setup-script
 omp_test = \
     r"""
@@ -64,6 +54,7 @@ def get_compiler_openmp_flag():
                 exit_code = subprocess.call([cc, '/openmp', filename], stdout=fnull, stderr=fnull)
                 if exit_code == 0:
                     openmp_flag = '/openmp'
+
     #clean up
     os.chdir(curdir)
     shutil.rmtree(tmpdir)
@@ -71,27 +62,31 @@ def get_compiler_openmp_flag():
 
 openmp_flag = get_compiler_openmp_flag()
 print 'get_compiler_openmp_flag():', openmp_flag
+extra_compile_args.append(openmp_flag)
+extra_link_args.append(openmp_flag)
 
-# --------------------- primesieve module ---------------------------
-
-primesieve_sources = libprimesieve_sources
-
-if cythonize is not None:
-    primesieve_sources.append("primesieve/core.pyx")
-else:
-    # fallback to compiled cpp
-    primesieve_sources.append("primesieve/core.cpp")
-
-extensions.append(Extension(
-    "primesieve.core",
-    primesieve_sources,
+library = ('primesieve', dict(
+    sources = glob("lib/primesieve/src/primesieve/*.cpp"),
     include_dirs = ["lib/primesieve/include"],
     language = "c++",
-    extra_compile_args = [openmp_flag],
-    extra_link_args = [openmp_flag]
+    extra_compile_args = extra_compile_args,
+    extra_link_args = extra_link_args
     ))
 
-# --------------------- Check if NumPy is installed -----------------
+if glob("primesieve/*.pyx"):
+    from Cython.Build import cythonize
+else:
+    # fallback to compiled cpp
+    cythonize = None
+
+extensions.append(Extension(
+        "primesieve.core",
+        ["primesieve/core.pyx"] if cythonize else ["primesieve/core.cpp"],
+        include_dirs = ["lib/primesieve/include"],
+        language = "c++",
+        extra_compile_args = extra_compile_args,
+        extra_link_args = extra_link_args
+        ))
 
 def is_pypy():
     import platform
@@ -114,33 +109,31 @@ def is_Numpy_installed():
         return False
     return bool(can_import("numpy"))
 
-# --------------------- primesieve.numpy module ---------------------
-
-primesieve_numpy_sources = libprimesieve_sources
-
-if cythonize is not None:
-    primesieve_numpy_sources.append("primesieve/numpy/core.pyx")
-else:
-    # fallback to compiled cpp
-    primesieve_numpy_sources.append("primesieve/numpy/core.cpp")
-
 # Add primesieve.numpy extension if NumPy is installed
 if is_Numpy_installed():
     import numpy
+    numpy_include_dir = numpy.get_include()
     extensions.append(Extension(
         "primesieve.numpy.core",
-        primesieve_numpy_sources,
-        include_dirs = ["lib/primesieve/include", numpy.get_include()],
+        ["primesieve/numpy/core.pyx"] if cythonize else ["primesieve/numpy/core.cpp"],
+        include_dirs = ["lib/primesieve/include", numpy_include_dir],
         language = "c++"
         ))
-
-# --------------------- Build extensions ----------------------------
 
 ext_modules = cythonize(extensions) if cythonize else [extensions]
 
 def old_msvc(compiler):
     """Test whether compiler is msvc <= 9.0"""
     return compiler.compiler_type == "msvc" and hasattr(compiler, "_MSVCCompiler__version") and int(compiler._MSVCCompiler__version) <= 9
+
+class build_clib_subclass(build_clib):
+    """Workaround to add msvc_compat (stdint.h) for old msvc versions"""
+    def build_libraries(self, libraries):
+        if old_msvc(self.compiler):
+            for lib_name, build_info in libraries:
+                build_info['include_dirs'].append("lib/primesieve/src/msvc_compat")
+                print(build_info)
+        build_clib.build_libraries(self, libraries)
 
 class build_ext_subclass(build_ext):
     """Workaround to add msvc_compat (stdint.h) for old msvc versions"""
@@ -156,9 +149,10 @@ setup(
     url = "https://github.com/hickford/primesieve-python",
     description = "Fast prime number generator. Python bindings for primesieve C/C++ library",
     license = "MIT",
+    libraries = [library],
     packages = ["primesieve", "primesieve.numpy"],
     ext_modules = ext_modules,
-    cmdclass = {'build_ext': build_ext_subclass},
+    cmdclass = {'build_ext': build_ext_subclass, 'build_clib' : build_clib_subclass},
     classifiers = [
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.6',
