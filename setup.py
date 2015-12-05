8c7813bd1015cb09d4c4903484e5ad01c4c78ab3
Kim Walisch <kim.walisch@gmail.com>
2015-12-05 16:48:12 +0100

    Don't depend on cython

--- setup.py
+++ setup.py
@@ -2,13 +2,21 @@ from setuptools import setup, Extension
 from glob import glob
 from distutils.command.build_ext import build_ext
 from distutils.command.build_clib import build_clib
-from Cython.Build import cythonize
 import distutils.sysconfig
 import platform
 import os
 import shutil
 import subprocess
 import tempfile
+
+# The primesieve package (https://pypi.python.org/pypi/primesieve)
+# distributes the generated C++ rather than the pyx file.
+# So Cython isn't needed as a dependency.
+if glob("primesieve/*.pyx"):
+    from Cython.Build import cythonize
+    module_file_ext = 'pyx'
+else:
+    module_file_ext = 'cpp'
 
 # --------------------- Initialization ------------------------------
 
@@ -33,7 +41,7 @@ omp_test = \
     """
 
 def get_compiler_openmp_flag():
-    openmp_flag = ""
+    openmp_flag = ''
     tmpdir = tempfile.mkdtemp()
     curdir = os.getcwd()
     os.chdir(tmpdir)
@@ -78,7 +86,7 @@ extra_link_args.append(openmp_flag)
 
 # ------------------ OpenMP hack for build_clib --------------------
 
-if openmp_flag != "":
+if openmp_flag != '':
     try:
         cflags = distutils.sysconfig.get_config_var('CFLAGS')
         distutils.sysconfig._config_vars['CFLAGS'] = cflags + " " + openmp_flag
@@ -121,7 +129,7 @@ libprimesieve = ('primesieve', dict(
 
 extensions.append(Extension(
     "primesieve._primesieve",
-    ["primesieve/_primesieve.pyx"],
+    ["primesieve/_primesieve." + module_file_ext],
     include_dirs = ["lib/primesieve/include"],
     extra_compile_args = extra_compile_args,
     extra_link_args = extra_link_args,
@@ -134,7 +142,7 @@ if is_Numpy_installed():
     import numpy
     extensions.append(Extension(
         "primesieve.numpy._generate",
-        ["primesieve/numpy/_generate.pyx"],
+        ["primesieve/numpy/_generate." + module_file_ext],
         include_dirs = ["lib/primesieve/include", numpy.get_include()],
         extra_compile_args = extra_compile_args,
         extra_link_args = extra_link_args,
@@ -143,18 +151,21 @@ if is_Numpy_installed():
 
 # --------------------- Build ---------------------------------------
 
-ext_modules = cythonize(extensions)
+if module_file_ext == 'pyx':
+    ext_modules = cythonize(extensions)
+elif module_file_ext:
+    ext_modules = extensions
 
 def old_msvc(compiler):
     """Test whether compiler is msvc <= 9.0"""
-    return compiler.compiler_type == "msvc" and hasattr(compiler, "_MSVCCompiler__version") and int(compiler._MSVCCompiler__version) <= 9
+    return compiler.compiler_type == 'msvc' and hasattr(compiler, '_MSVCCompiler__version') and int(compiler._MSVCCompiler__version) <= 9
 
 class build_clib_subclass(build_clib):
     """Workaround to add msvc_compat (stdint.h) for old msvc versions"""
     def build_libraries(self, libraries):
         if old_msvc(self.compiler):
             for lib_name, build_info in libraries:
-                build_info['include_dirs'].append("lib/primesieve/src/msvc_compat")
+                build_info['include_dirs'].append('lib/primesieve/src/msvc_compat')
                 print(build_info)
         build_clib.build_libraries(self, libraries)
 
@@ -163,17 +174,17 @@ class build_ext_subclass(build_ext):
     def build_extensions(self):
         if old_msvc(self.compiler):
             for e in self.extensions:
-                e.include_dirs.append("lib/primesieve/src/msvc_compat")
+                e.include_dirs.append('lib/primesieve/src/msvc_compat')
         build_ext.build_extensions(self)
 
 setup(
     name = 'primesieve',
-    version = "1.0.0",
-    url = "https://github.com/hickford/primesieve-python",
-    description = "Fast prime number generator. Python bindings for primesieve C/C++ library",
-    license = "MIT",
+    version = '1.0.0',
+    url = 'https://github.com/hickford/primesieve-python',
+    description = 'Fast prime number generator. Python bindings for primesieve C/C++ library',
+    license = 'MIT',
     libraries = [libprimesieve],
-    packages = ["primesieve", "primesieve.numpy"],
+    packages = ['primesieve', 'primesieve.numpy'],
     ext_modules = ext_modules,
     cmdclass = {'build_ext': build_ext_subclass, 'build_clib' : build_clib_subclass},
     classifiers = [
