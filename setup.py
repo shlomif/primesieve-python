5cd182df56dc850adc8caf681154fa45bd6bbf80
Kim Walisch <kim.walisch@gmail.com>
2017-03-22 19:48:27 +0100

    Parallel C++ compilation

--- setup.py
+++ setup.py
@@ -130,6 +130,35 @@ if is_Numpy_installed():
         language = "c++"
         ))
 
+# --------------------- Parallel build -------------------------------
+# http://stackoverflow.com/a/13176803/363778
+
+def parallel_cpp_compile(self, 
+                         sources, 
+                         output_dir=None, 
+                         macros=None, 
+                         include_dirs=None, 
+                         debug=0, 
+                         extra_preargs=None, 
+                         extra_postargs=None, 
+                         depends=None):
+
+    macros, objects, extra_postargs, pp_opts, build = \
+        self._setup_compile(output_dir, macros, include_dirs, sources, depends, extra_postargs)
+    cc_args = self._get_cc_args(pp_opts, debug, extra_preargs)
+    import multiprocessing.pool
+    def _single_compile(obj):
+        try:
+            src, ext = build[obj]
+        except KeyError:
+            return
+        self._compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
+    list(multiprocessing.pool.ThreadPool().imap(_single_compile,objects))
+    return objects
+
+import distutils.ccompiler
+distutils.ccompiler.CCompiler.compile = parallel_cpp_compile
+
 # --------------------- Build ---------------------------------------
 
 if module_file_ext == 'pyx':
