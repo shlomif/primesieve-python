693c6110d4163921cd473f0f466e76bc4e619f5b
Shlomi Fish <shlomif@shlomifish.org>
2019-07-06 20:58:25 +0300

    update docs and identifiers

--- primesieve/array/_array.pyx
+++ primesieve/array/_array.pyx
@@ -10,14 +10,14 @@ cdef extern from "primesieve.h":
 cdef extern from 'errno.h':
     int errno
 
-cdef c_to_numpy_array(void* ptr, size_t N):
-    """Bind C array allocated using malloc to NumPy ndarray"""
+cdef c_to_array_array(void* ptr, size_t N):
+    """Bind C array allocated using malloc to python array.array"""
     arr = array.array('L', [])
     arr.frombytes((<char*>ptr)[:(N*sizeof(uint64_t))])
     return arr
 
 cpdef primes(int64_t a, int64_t b = 0):
-    """Generate a numpy primes array"""
+    """Generate a primes array.array"""
     a = max(a, 0)
     b = max(b, 0)
     if b == 0:
@@ -33,11 +33,11 @@ cpdef primes(int64_t a, int64_t b = 0):
     if errno != 0:
         raise RuntimeError("Failed generating primes, most likely due to insufficient memory.")
 
-    primes = c_to_numpy_array(c_primes, size)
+    primes = c_to_array_array(c_primes, size)
     return primes
 
 cpdef n_primes(int64_t n, int64_t start = 0):
-    """Generate a numpy array with the next n primes"""
+    """Generate an array.array with the next n primes"""
     n = max(n, 0)
     start = max(start, 0)
 
@@ -50,5 +50,5 @@ cpdef n_primes(int64_t n, int64_t start = 0):
     if errno != 0:
         raise RuntimeError("Failed generating primes, most likely due to insufficient memory.")
 
-    primes = c_to_numpy_array(c_primes, n)
+    primes = c_to_array_array(c_primes, n)
     return primes
