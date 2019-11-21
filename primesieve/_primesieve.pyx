3a8edfe331f90de478c31ff41f662e54a36a2282
Brian Larsen <B_R_L@hotmail.com>
2019-11-21 11:20:34 -0600

    Add error handling to C API calls.

--- primesieve/_primesieve.pyx
+++ primesieve/_primesieve.pyx
@@ -91,105 +91,254 @@ cpdef n_primes(uint64_t n, uint64_t start = 0) except +:
     cpp_primesieve.primesieve_free(c_primes)
     return primes
 
-cpdef uint64_t nth_prime(int64_t n, uint64_t start = 0) except +:
-    """Find the nth prime > start"""
-    return cpp_primesieve.primesieve_nth_prime(n, start)
 
-cpdef uint64_t count_primes(uint64_t a, uint64_t b = 0) except +:
+cpdef uint64_t nth_prime(int64_t n, uint64_t start = 0) except -1:
+    """Find the nth prime > start"""
+
+    global errno
+    errno = 0
+
+    cdef uint64_t result = cpp_primesieve.primesieve_nth_prime(n, start)
+
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
+
+    return result
+
+cpdef uint64_t count_primes(uint64_t a, uint64_t b = 0) except -1:
     """Count prime numbers"""
     if b == 0:
         (a,b) = (0,a)
-    return cpp_primesieve.primesieve_count_primes(a, b)
 
-cpdef uint64_t count_twins(uint64_t a, uint64_t b = 0) except +:
+    global errno
+    errno = 0
+
+    cdef uint64_t result = cpp_primesieve.primesieve_count_primes(a, b)
+
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
+
+    return result
+
+cpdef uint64_t count_twins(uint64_t a, uint64_t b = 0) except -1:
     """Count twin primes """
     if b == 0:
         (a,b) = (0,a)
-    return cpp_primesieve.primesieve_count_twins(a, b)
 
-cpdef uint64_t count_triplets(uint64_t a, uint64_t b = 0) except +:
+    global errno
+    errno = 0
+
+    cdef uint64_t result = cpp_primesieve.primesieve_count_twins(a, b)
+
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
+
+    return result
+
+cpdef uint64_t count_triplets(uint64_t a, uint64_t b = 0) except -1:
     """Count prime triplets """
     if b == 0:
         (a,b) = (0,a)
-    return cpp_primesieve.primesieve_count_triplets(a, b)
 
-cpdef uint64_t count_quadruplets(uint64_t a, uint64_t b = 0) except +:
+    global errno
+    errno = 0
+
+    cdef uint64_t result = cpp_primesieve.primesieve_count_triplets(a, b)
+
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
+
+    return result
+
+cpdef uint64_t count_quadruplets(uint64_t a, uint64_t b = 0) except -1:
     """Count prime quadruplets """
     if b == 0:
         (a,b) = (0,a)
-    return cpp_primesieve.primesieve_count_quadruplets(a, b)
 
-cpdef uint64_t count_quintuplets(uint64_t a, uint64_t b = 0) except +:
+    global errno
+    errno = 0
+
+    cdef uint64_t result = cpp_primesieve.primesieve_count_quadruplets(a, b)
+
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
+
+    return result
+
+cpdef uint64_t count_quintuplets(uint64_t a, uint64_t b = 0) except -1:
     """Count prime quintuplets """
     if b == 0:
         (a,b) = (0,a)
-    return cpp_primesieve.primesieve_count_quintuplets(a, b)
 
-cpdef uint64_t count_sextuplets(uint64_t a, uint64_t b = 0) except +:
+    global errno
+    errno = 0
+
+    cdef uint64_t result = cpp_primesieve.primesieve_count_quintuplets(a, b)
+
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
+
+    return result
+
+cpdef uint64_t count_sextuplets(uint64_t a, uint64_t b = 0) except -1:
     """Count prime sextuplets """
     if b == 0:
         (a,b) = (0,a)
-    return cpp_primesieve.primesieve_count_sextuplets(a, b)
 
-cpdef void print_primes(uint64_t a, uint64_t b = 0) except +:
+    global errno
+    errno = 0
+
+    cdef uint64_t result = cpp_primesieve.primesieve_count_sextuplets(a, b)
+
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
+
+    return result
+
+cpdef uint64_t get_max_stop() except -1:
+    """Returns the largest valid stop number for primesieve. 2^64-1 (UINT64_MAX)"""
+
+    global errno
+    errno = 0
+
+    cdef uint64_t result = cpp_primesieve.primesieve_get_max_stop()
+
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
+
+    return result
+
+cpdef int get_sieve_size() except -1:
+    """Get the current set sieve size in KiB"""
+
+    global errno
+    errno = 0
+
+    cdef int result = cpp_primesieve.primesieve_get_sieve_size()
+
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
+
+    return result
+
+cpdef int get_num_threads() except -1:
+    """Get the currently set number of threads"""
+
+    global errno
+    errno = 0
+
+    cdef int result = cpp_primesieve.primesieve_get_num_threads()
+
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
+
+    return result
+
+cpdef string primesieve_version():
+    """Get the primesieve version number"""
+    return cpp_primesieve.primesieve_version()
+
+cpdef void print_primes(uint64_t a, uint64_t b = 0) except *:
     """Print prime numbers to stdout"""
     if b == 0:
         (a,b) = (0,a)
+
+    global errno
+    errno = 0
+
     cpp_primesieve.primesieve_print_primes(a, b)
 
-cpdef void print_twins(uint64_t a, uint64_t b = 0) except +:
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
+
+cpdef void print_twins(uint64_t a, uint64_t b = 0) except *:
     """Print twin primes to stdout"""
     if b == 0:
         (a,b) = (0,a)
+
+    global errno
+    errno = 0
+
     cpp_primesieve.primesieve_print_twins(a, b)
 
-cpdef void print_triplets(uint64_t a, uint64_t b = 0) except +:
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
+
+cpdef void print_triplets(uint64_t a, uint64_t b = 0) except *:
     """Print prime triplets to stdout"""
     if b == 0:
         (a,b) = (0,a)
+
+    global errno
+    errno = 0
+
     cpp_primesieve.primesieve_print_triplets(a, b)
 
-cpdef void print_quadruplets(uint64_t a, uint64_t b = 0) except +:
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
+
+cpdef void print_quadruplets(uint64_t a, uint64_t b = 0) except *:
     """Print prime quadruplets to stdout"""
     if b == 0:
         (a,b) = (0,a)
+
+    global errno
+    errno = 0
+
     cpp_primesieve.primesieve_print_quadruplets(a, b)
 
-cpdef void print_quintuplets(uint64_t a, uint64_t b = 0) except +:
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
+
+cpdef void print_quintuplets(uint64_t a, uint64_t b = 0) except *:
     """Print prime quintuplets to stdout"""
     if b == 0:
         (a,b) = (0,a)
+
+    global errno
+    errno = 0
+
     cpp_primesieve.primesieve_print_quintuplets(a, b)
 
-cpdef void print_sextuplets(uint64_t a, uint64_t b = 0) except +:
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
+
+cpdef void print_sextuplets(uint64_t a, uint64_t b = 0) except *:
     """Print prime sextuplets to stdout"""
     if b == 0:
         (a,b) = (0,a)
+
+    global errno
+    errno = 0
+
     cpp_primesieve.primesieve_print_sextuplets(a, b)
 
-cpdef uint64_t get_max_stop() except +:
-    """Returns the largest valid stop number for primesieve. 2^64-1 (UINT64_MAX)"""
-    return cpp_primesieve.primesieve_get_max_stop()
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
 
-cpdef int get_sieve_size() except +:
-    """Get the current set sieve size in KiB"""
-    return cpp_primesieve.primesieve_get_sieve_size()
-
-cpdef int get_num_threads() except +:
-    """Get the currently set number of threads"""
-    return cpp_primesieve.primesieve_get_num_threads()
-
-cpdef void set_sieve_size(int sieve_size) except +:
+cpdef void set_sieve_size(int sieve_size) except *:
     """Set the sieve size in KiB (kibibyte).  The best sieving performance is achieved with a sieve size of your CPU's L1 or L2 cache size (per core)."""
+
+    global errno
+    errno = 0
+
     cpp_primesieve.primesieve_set_sieve_size(sieve_size)
 
-cpdef void set_num_threads(int threads) except +:
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
+
+cpdef void set_num_threads(int threads) except *:
     """Set the number of threads for sieving"""
+
+    global errno
+    errno = 0
+
     cpp_primesieve.primesieve_set_num_threads(threads)
 
-cpdef string primesieve_version() except +:
-    """Get the primesieve version number"""
-    return cpp_primesieve.primesieve_version()
+    if errno != 0:
+        raise RuntimeError("Primesieve ran into an error.")
+
+
 
 cdef class Iterator:
     cdef cpp_primesieve.iterator _iterator
