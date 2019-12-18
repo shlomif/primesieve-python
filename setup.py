9d1558100b7e2a4a696bdf07f7edb5824e55c64f
kimwalisch <kim.walisch@gmail.com>
2019-12-18 16:41:20 +0100

    Update version to 2.4

--- setup.py
+++ setup.py
@@ -180,7 +180,7 @@ else:
 
 setup(
     name='primesieve',
-    version='2.0.3',
+    version='2.0.4',
     url='https://github.com/kimwalisch/primesieve-python',
     long_description=open('README.md', "rb").read().decode('utf8'),
     long_description_content_type='text/markdown',
