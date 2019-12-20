9097bd353956a739a164454d25b492cefa7391ce
kimwalisch <kim.walisch@gmail.com>
2019-12-20 22:27:37 +0100

    Increase version number

--- setup.py
+++ setup.py
@@ -180,7 +180,7 @@ else:
 
 setup(
     name='primesieve',
-    version='2.0.9',
+    version='2.0.10',
     url='https://github.com/kimwalisch/primesieve-python',
     long_description=open('README.md', "rb").read().decode('utf8'),
     long_description_content_type='text/markdown',
