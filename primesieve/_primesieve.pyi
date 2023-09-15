8615e99ddf111716ccd9b212716fc1686e0342e1
PaNDa2code <moaaz0688@gmail.com>
2023-09-15 20:06:18 +0000

    added stub files for the moudles and doc for primes funtion

---
+++ primesieve/_primesieve.pyi
@@ -0,0 +1,19 @@
+import primesieve
+import array
+
+def primes() -> array.array:
+    """
+    ## funciton to genrate a array of primes numpers less than the inputed argumnt
+
+    #### input: int
+    #### returns: array
+    """
+    pass
+
+def n_primes() -> array.array:
+    """
+    function to genreate a array of prime numpers with the size of input N
+    """
+    pass
+
+def numpy.primes():
---
+++ primesieve/numpy/_numpy.pyi
@@ -0,0 +1,22 @@
+import types
+import numpy as np
+
+def primes() -> np.ndarray[np.int_,None]:
+    """
+    ### funciton to genrate a array of primes numpers less than the inputed argumnt
+
+    input: int
+    returns: numpy array 'ndarray'
+    """
+    pass
+
+def n_primes() -> np.ndarray[np.int_,None]:
+    """
+    ### function to genreate a array of prime numpers with the size of input N
+
+    input: int
+    returns: numpy array 'ndarray'
+    """
+    pass
+
+
