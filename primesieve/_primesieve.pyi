922a12be180e0a8ed7cb8477dfada5940fde4c09
PaNDa2code <moaaz0688@gmail.com>
2023-09-17 23:34:39 +0000

    done with `Iterator` object docs

--- primesieve/_primesieve.pyi
+++ primesieve/_primesieve.pyi
@@ -1,20 +1,98 @@
 from typing import List, Union
 from libc.stdint import uint64_t, int64_t
 
-# Define custom types for your extension
-_arr_type = Union[bytes, bytearray]
 
 class Iterator:
+    """
+    Iterator is an object that allows you to navigate between prime numbers (forward or backward).
+
+    Methods:
+        - `skipto(start: int, stop_hint: int = 2**62) -> None`: Skip to the specified number or the nearest prime number.
+        - `next_prime() -> int`: Get the next prime number in the sequence.
+        - `prev_prime() -> int`: Get the previous prime number in the sequence.
+
+
+    Example:
+        ```
+        >>> import primesieve
+        >>> from primesieve import Iterator
+        >>> # Try it and you will git it 
+        >>> number = 225627040
+        >>> I = Iterator()
+        >>> I.skipto(number)
+        >>> I.next_prime()
+        225627049
+        >>> I.prev_prime()
+        225627037
+        >>> 
+        ```
+    """
     def __init__(self) -> None:
         ...
 
     def skipto(self, start: int, stop_hint: int = 2**62) -> None:
+        """
+        Skip to the specified number or the nearest prime number.
+        
+        Arguments:
+        
+            :param start: The target number or a number before the target prime.
+            :param stop_hint: An optional hint for the upper limit.
+
+            
+        Example:
+            ```
+            >>> import primesieve
+            >>> from primesieve import Iterator
+
+            >>> number = 225627040
+            >>> I = Iterator()
+            >>> I.skipto(number)
+            >>> I.next_prime()
+            225627049
+            ```
+        """
         ...
 
+
     def next_prime(self) -> int:
+        """
+        Get the next prime number in the sequence.
+
+        :return: The next prime number.
+
+        Example:
+            ```
+            >>> import primesieve
+            >>> from primesieve import Iterator
+
+            >>> number = 225627040
+            >>> I = Iterator()
+            >>> I.skipto(number)
+            >>> I.next_prime()
+            225627049
+            ```
+        """
         ...
 
     def prev_prime(self) -> int:
+        """
+        Get the previous prime number in the sequence.
+
+        :return: The previous prime number.
+
+        Example:
+            ```
+            >>> import primesieve
+            >>> from primesieve import Iterator
+
+            >>> number = 225627040
+            >>> I = Iterator()
+            >>> I.skipto(number)
+            >>> I.prev_prime()
+            225627037
+            ```
+        """
         ...
 
 def primes(from_limit: uint64_t, to_limit: uint64_t = 0) -> List[int]:
