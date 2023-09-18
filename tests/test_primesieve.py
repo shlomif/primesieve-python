2c50bca8a48fd6a26b9a18a9e18aa994ec7208c1
PaNDa2code <moaaz0688@gmail.com>
2023-09-18 10:24:06 +0000

    added test funciton fo `primes_range` iterator

--- tests/test_primesieve.py
+++ tests/test_primesieve.py
@@ -54,3 +54,8 @@ def test_n_primes_array():
     assert_array_equal(primesieve.n_primes(7),
                        [2, 3, 5, 7, 11, 13, 17])
     assert_array_equal(primesieve.n_primes(5, 100), [101, 103, 107, 109, 113])
+
+
+def test_primes_range():
+    """Test primes_range iterator output."""
+    assert_array_equal(list(primesieve.primes_range(10,100)), [11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
