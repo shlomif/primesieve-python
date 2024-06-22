128583834dedc08b833f1735fd477616d092a029
Shlomi Fish <shlomif@shlomifish.org>
2024-06-22 09:40:57 +0300

    add a [currently non-functional] docker-ci script

---
+++ docker-ci-run.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2024 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Licensed under the terms of the MIT license.
+
+"""
+
+Work-in-Progress docker-based CI program.
+
+"""
+
+import docker
+client = docker.from_env()
+images = client.images
+if False:
+    image = images.pull('fedora:40')
+    images.prune(image)
+container = client.containers.run('fedora:40', detach=True)
+listt = client.containers.list()
+print(listt)
